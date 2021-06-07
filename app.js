4ecf4d0a6f7efdc7b1073f9d0d074808268c9c07
r-bota <76598683+r-bota@users.noreply.github.com>
2021-06-07 14:55:48 -0700

    Js for desktop site

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var nav = document.getElementById("nav")
 var closenav = document.getElementById("closenav")
 var left = document.getElementById("left")
 var right = document.getElementById("right")
+var dtLeft = document.getElementById("desktopleft")
+var dtRight = document.getElementById("desktopright")
 var hero = document.getElementById("heropic")
 var picNum = 1;
 
@@ -28,4 +30,20 @@ left.addEventListener("click", () => {
         picNum = 3;
     }
     hero.style.backgroundImage = `url(images/mobile-image-hero-${picNum}.jpg)`;
+})
+
+dtRight.addEventListener("click", () => {
+    picNum += 1;
+    if (picNum === 4) {
+        picNum = 1;
+    }
+    hero.style.backgroundImage = `url(images/desktop-image-hero-${picNum}.jpg)`;
+})
+
+dtLeft.addEventListener("click", () => {
+    picNum += 1;
+    if (picNum === 4) {
+        picNum = 1;
+    }
+    hero.style.backgroundImage = `url(images/desktop-image-hero-${picNum}.jpg)`;
 })
